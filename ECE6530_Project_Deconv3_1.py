6e3cd129dee3853938638a9ff4adef16f6c1dcf7
Thomas Warren <148013308+thomasjw99@users.noreply.github.com>
2023-11-22 16:30:43 -0700

    On to 3.1.2
    Fixed Restoration filter, working on worst error

--- ECE6530_Project_Deconv3_1.py
+++ ECE6530_Project_Deconv3_1.py
@@ -1,23 +1,45 @@
 import matplotlib.pyplot as plt     ## Libary used for displaying plots and formatting plots
 import numpy as np                  ## Library used for arrays functions, mathematical functions, etc.
-from matplotlib import cm
-import math
-import cmath
-from scipy.fft import fft,ifft
+# from matplotlib import cm
+# import math
+# import cmath
+# from scipy.fft import fft,ifft
 # import sys
 np.set_printoptions(threshold=np.inf)
-import unicodedata
+# import unicodedata
+ 
 
+def zeropad(x,y):
+    xpad = np.zeros((len(x)+2*len(y)-2),dtype=np.complex128)
+    ylength = len(y)-1
+    for ii in range(0,len(x)):
+        xpad[ii + ylength] = x[ii]
+    return xpad
+ 
 
+def convo(x,b):
+    bk = b
+    x_n = zeropad(x,bk)
+    y_padd = np.zeros(len(x_n),dtype=np.complex128)
+    y_n = np.zeros(len(x),dtype=np.complex128)
+    b_ind = len(b)-1
+    for n in range(0,len(x_n)):
+        for k in range(0, len(bk)):
+            y_padd[n] += (bk[k]*x_n[n-k])
+ 
+    for ii in range(0,len(x)):
+        y_n[ii] = y_padd[ii+b_ind]
+    return y_n
+ 
 def firconv(p):
     x_n = 256*((np.arange(0,100,1)%50)<10)
-    bk = [1,0.9]
-    w_n = np.convolve(x_n,bk)
+    bk = [1,-0.9]
+    w_n = convo(x_n,bk)
     n = np.arange(0,100)
     if p == 0:
         print(x_n)
-        print(w_n)                                                                     ########FIX 3.1 a) Text response########
-        print("3.1 a) I am unsure of what the effect of these filter coefficients is, will read into.")       
+        print(w_n)
+        print("3.1 a) I am unsure of what the effect of these filter coefficients is")
         print("3.1 b) The length of the filtered signal is the length of x_n added to the length of bk minus 1.")
         print("The nonzero portion of w_n is one value longer than x_n as well, producing 11 vales rather than 10.")
         print("length of x_n is ", len(x_n), "samples")
@@ -35,40 +57,56 @@ def firconv(p):
         axs[1].grid()
         plt.show()
     return w_n,x_n
-    
-def restor():
-    w_n,x_n = firconv(1)
-    # print(w_n)
-    # print(x_n)
-    r = 0.9
+   
+def restor(w_n,x_n):
     M = 22
-    l = np.arange(0,M)
-    n = np.arange(len(w_n))
-    y_sum = np.zeros(len(w_n), dtype=np.complex128)
+    r = 0.9
     y_n = np.zeros(len(w_n),dtype=np.complex128)
-    
-    for ii in range(0,M):
-        for k in range(0,len(w_n)):
-            y_sum[k] = (r**ii)*w_n[k-ii]
-        # print("ii = ",ii)
-        # print(y_sum)
-        y_n[ii] = np.sum(y_sum)
-    
-    fig, axs = plt.subplots(2,1, figsize = (12,12))
-    axs[0].stem(n,w_n, markerfmt = 'red')
-    axs[0].title.set_text("w(n)")
+   
+    for n in range(0,len(w_n)):
+        for l in range(0, M):
+            y_n[n] += (r**l)*w_n[n-l]
+   
+def restor_plot(w_n,x_n):
+   
+    M = 22
+    r = 0.9
+    n_val = np.arange(len(w_n))
+    y_n = np.zeros(len(w_n),dtype=np.complex128)
+    y_err = np.zeros(len(w_n),dtype=np.complex128)
+   
+    for n in range(0,len(w_n)):
+        for l in range(0, M):
+            y_n[n] += (r**l)*w_n[n-l]
+    for ii in range(0,len(x_n)):
+        y_err[ii] = y_n[ii] - x_n[ii]
+ 
+    fig, axs = plt.subplots(4,1, figsize = (12,12))
+    axs[0].stem(n_val,x_n)
     axs[0].grid()
-    axs[0].set_ylabel("w(n)")
+    axs[0].set_ylabel("x(n)")
     axs[0].set_xlabel("Samples")
-    axs[1].stem(n,y_n)
-    axs[1].title.set_text("y(n)")
-    axs[1].set_ylabel("y(n)")
-    axs[1].set_xlabel("samples")
+    axs[1].stem(n_val,w_n)
     axs[1].grid()
+    axs[1].set_ylabel("w(n)")
+    axs[1].set_xlabel("Samples")
+    axs[2].stem(n_val,y_n)
+    axs[2].set_ylabel("y(n)")
+    axs[2].set_xlabel("samples")
+    axs[2].grid()
+    axs[3].stem(n_val[0:50],y_err[0:50])
+    axs[3].set_ylabel("error y(n) - x(n)")
+    axs[3].set_xlabel("samples")
+    axs[3].grid()
+   
     plt.show()
-    
+   
+   
+   
 def main():
-    # firconv("no")
-    restor()
+    w_n,x_n = firconv(1)
+    # firconv(1")
+    # firconv(0)
+    restor_plot(w_n,x_n)
 if __name__== "__main__":
     main()
